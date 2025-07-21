f637ba8146b8c72f73b6abb5fb3d931926880f11
Amish <amishkumardubey@gmail.com>
2025-07-21 16:55:44 +0530

    Create WhyChooseUs and FeaturedCourses

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import HeroSection from "@/components/HeroSection";
 
 import { Spotlight } from '@/components/ui/Spotlight'
+import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
@@ -11,7 +13,9 @@ export default function Home() {
         className="-top-40 left-0 md:-top-20 md:left-60"
         fill="white"
       />
-      <HeroSection/>     
+      <HeroSection/> 
+      <FeaturedCourses/>
+      <WhyChooseUs/>    
       </main>
   );
 }
